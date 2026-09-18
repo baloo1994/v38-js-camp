efcb50b5c7e9ae2c364f8c5be5902b50792624e2
Dino Franzén <dino.franzen@gmail.com>
2026-09-18 08:57:47 +0200

    feat: introduce const/let and basic types

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-console.log("Nord Glow");
-console.log("Nord Glow");
-console.log("Nord Glow");
+const eventName = "Tallest man on Earth";
+let ticketCount = 0;
+
+console.log(eventName);
+console.log(eventName);
+ticketCount = 10;
+console.log(ticketCount);
+
+const isVip = false;
+console.log(isVip);
