63099576362bbaff25bf9e5755e7fb595afad34d
Dino Franzén <dino.franzen@gmail.com>
2026-09-18 08:59:59 +0200

    fixade formatering och kommentar

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const eventName = "Tallest man on Earth";
 let ticketCount = 0;
+const isVip = false;
 
 console.log(eventName);
 console.log(eventName);
+
 ticketCount = 10;
 console.log(ticketCount);
-
-const isVip = false;
 console.log(isVip);
+
+// eventName="Nord Glow"; // TypeError behåll bortkommenterad
